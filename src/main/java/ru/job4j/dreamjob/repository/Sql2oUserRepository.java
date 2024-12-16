951b173e834863010c0f5a262e34d4bec5c34307
YuryGe <postbox177@yandex.ru>
2024-12-16 08:55:37 +0400

    4. Многопоточность в базе данных - исправление 2

--- src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
@@ -30,7 +30,7 @@ public class Sql2oUserRepository implements UserRepository {
             user.setId(generatedId);
             return Optional.of(user);
         } catch (RuntimeException e) {
-            LOGGER.error(e.getMessage());
+            LOGGER.error(e.getMessage(), e);
         }
         return Optional.empty();
     }
